250154abe25983011d13ff919a93ccf11aa4f324
haithamassoli <haitham.b.assoli@gmail.com>
2021-12-03 01:32:08 +0200

    styling dashboard

---
+++ code/admin/includes/footer.php
@@ -0,0 +1,36 @@
+    <!-- ! Footer -->
+    <footer class="footer">
+      <div class="container footer--flex">
+        <div class="footer-start">
+          <p>2021 © Elegant Dashboard - <a href="elegant-dashboard.com" target="_blank" rel="noopener noreferrer">elegant-dashboard.com</a></p><br>
+          <p>Distributed by - <a href="https://themewagon.com/" target="_blank" rel="noopener noreferrer">Themewagon</a></p>
+        </div>
+        <ul class="footer-end">
+          <li><a href="##">About</a></li>
+          <li><a href="##">Support</a></li>
+          <li><a href="##">Puchase</a></li>
+        </ul>
+      </div>
+    </footer>
+    </div>
+    </div>
+    <!-- Chart library -->
+    <script src="assets/plugins/chart.min.js"></script>
+    <!-- Icons library -->
+    <script src="assets/plugins/feather.min.js"></script>
+    <!-- Custom scripts -->
+    <script src="assets/js/script.js"></script>
+    <script src="assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+
+    <script src="assets/vendors/simple-datatables/simple-datatables.js"></script>
+    <script>
+      // Simple Datatable
+      let table1 = document.querySelector('#table1');
+      let dataTable = new simpleDatatables.DataTable(table1);
+    </script>
+
+    <script src=/assets/js/main.js"></script>
+    </body>
+
+    </html>
