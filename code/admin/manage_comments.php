df50aee91aaf6a11e944e776bc82b26bb5ba3e51
haithamassoli <haitham.b.assoli@gmail.com>
2021-12-03 10:11:44 +0200

    finished manage comments page

--- code/admin/manage_comments.php
+++ code/admin/manage_comments.php
@@ -1,259 +1,131 @@
-<?php include "./includes/header.php"; ?>
-<link rel="stylesheet" href="assets/vendors/simple-datatables/style.css">
-<link rel="stylesheet" href="assets/vendors/perfect-scrollbar/perfect-scrollbar.css">
-<link rel="stylesheet" href="assets/vendors/bootstrap-icons/bootstrap-icons.css">
-<link rel="stylesheet" href="assets/css/app.css">
-<link rel="shortcut icon" href="assets/images/favicon.svg" type="image/x-icon">
+<?php include "./includes/header.php";
+require "./includes/connect.php";
+// select all comments
+$sql = "SELECT comments.*, products.product_name, users.user_name, users.user_image FROM comments INNER JOIN products ON products.product_id = comments.comment_product_id INNER JOIN users ON users.user_id = comments.comment_user_id";
+$result = mysqli_query($conn, $sql);
+$comments = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+if (isset($_GET["do"])) {
+  $do = $_GET["do"];
+  $id = $_GET["id"];
+  // delete
+  if ($do == "delete") {
+    $sql = "DELETE FROM comments WHERE comment_id = '$id'";
+    $result = mysqli_query($conn, $sql);
+  }
+  // edit
+  // if ($do == "edit") {
+  //   $sql = "UPDATE comments SET column1=value, column2=value2,...
+  //   WHERE some_column=some_value  = '$id'";
+  //   $result = mysqli_query($conn, $sql);
+  // }
+  //   add
+  // if ($do == "edit") {
+  //   $sql = "DELETE FROM comments WHERE comment_id = '$id'";
+  //   $result = mysqli_query($conn, $sql);
+  // }
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  } ?>
+  <!-- start form -->
+  <div class="col-md-6 col-12">
+    <div class="card">
+      <div class="card-header">
+        <h4 class="card-title">Horizontal Form with Icons</h4>
+      </div>
+      <div class="card-content">
+        <div class="card-body">
+          <form class="form form-horizontal" method="POST" enctype="multipart/form-data">
+            <div class="form-body">
+              <div class="row">
+                <div class="col-md-4">
+                  <label>Comment</label>
+                </div>
+                <div class="col-md-8">
+                  <div class="form-group has-icon-left">
+                    <div class="position-relative row justify-content-center align-items-center d-flex">
+                      <input name="comment" type="text" class="form-control col-9 mb-2" placeholder="Name" style="border: 1px solid #dce7f1 !important;" id="first-name-icon">
+                      <div class="form-control-icon col-3 ">
+                        <i class="bi bi-person" style="position: absolute; top:-10px; left: -20px;"></i>
+                      </div>
+                      <span class="text-danger mb-2"><?php echo isset($commentError) ? $commentError : ""; ?></span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4">
+                  <label>Image</label>
+                </div>
+                <div class="col-md-8">
+                  <div class="form-group has-icon-left">
+                    <div class="position-relative row justify-content-center align-items-center d-flex">
+                      <input name="image" type="file" class="form-control col-9 mb-2" style="border: 1px solid #dce7f1 !important;" id="first-name-icon">
+                      <div class="form-control-icon col-3">
+                        <i class="bi bi-envelope" style="position: absolute; top:-10px; left: -20px;"></i>
+                      </div>
+                      <span class="text-danger"><?php echo isset($imageError) ? $imageError : ""; ?></span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-12 d-flex justify-content-end">
+                  <button type="submit" class="btn btn-primary me-1 mb-1"><?php echo $_GET["do"] == "edit" ? "Save" : "Add"; ?></button>
+                  <button type="reset" class="btn btn-light-secondary me-1 mb-1">Reset</button>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end form -->
+<?php  } ?>
+
+<!-- start table -->
 <div class="row">
-  <div class="col-lg-12">
+  <div class="offset-2 col-lg-9">
     <div class="users-table table-wrapper">
-      <table class="table table-striped" id="table1">
+      <table class="table table-striped" style="border: 2px solid #dce7f1 ;" id="table1">
         <thead>
           <tr class="users-table-info">
-            <th>
-              <label class="users-table__checkbox ms-20">
-                <input type="checkbox" class="check-all">Thumbnail
-              </label>
-            </th>
-            <th>Title</th>
-            <th>Author</th>
-            <th>Status</th>
-            <th>Date</th>
-            <th>Action</th>
+            <th>User</th>
+            <th>Comment</th>
+            <th>image</th>
+            <th>date created</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <label class="users-table__checkbox">
-                <input type="checkbox" class="check">
-                <div class="categories-table-img">
-                  <picture>
-                    <source srcset="./img/categories/01.webp" type="image/webp"><img src="./img/categories/01.jpg" alt="category">
-                  </picture>
-                </div>
-              </label>
-            </td>
-            <td>
-              Starting your traveling blog with Vasco
-            </td>
-            <td>
-              <div class="pages-table-img">
-                <picture>
-                  <source srcset="./img/avatar/avatar-face-04.webp" type="image/webp"><img src="./img/avatar/avatar-face-04.png" alt="User Name">
-                </picture>
-                Jenny Wilson
-              </div>
-            </td>
-            <td><span class="badge-pending">Pending</span></td>
-            <td>17.04.2021</td>
-            <td>
-              <span class="p-relative">
-                <button class="dropdown-btn transparent-btn" type="button" title="More info">
-                  <div class="sr-only">More info</div>
-                  <i data-feather="more-horizontal" aria-hidden="true"></i>
-                </button>
-                <ul class="users-item-dropdown dropdown">
-                  <li><a href="##">Edit</a></li>
-                  <li><a href="##">Quick edit</a></li>
-                  <li><a href="##">Trash</a></li>
-                </ul>
-              </span>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <label class="users-table__checkbox">
-                <input type="checkbox" class="check">
-                <div class="categories-table-img">
-                  <picture>
-                    <source srcset="./img/categories/02.webp" type="image/webp"><img src="./img/categories/02.jpg" alt="category">
-                  </picture>
-                </div>
-              </label>
-            </td>
-            <td>
-              Start a blog to reach your creative peak
-            </td>
-            <td>
-              <div class="pages-table-img">
-                <picture>
-                  <source srcset="./img/avatar/avatar-face-03.webp" type="image/webp"><img src="./img/avatar/avatar-face-03.png" alt="User Name">
-                </picture>
-                Annette Black
-              </div>
-            </td>
-            <td><span class="badge-pending">Pending</span></td>
-            <td>23.04.2021</td>
-            <td>
-              <span class="p-relative">
-                <button class="dropdown-btn transparent-btn" type="button" title="More info">
-                  <div class="sr-only">More info</div>
-                  <i data-feather="more-horizontal" aria-hidden="true"></i>
-                </button>
-                <ul class="users-item-dropdown dropdown">
-                  <li><a href="##">Edit</a></li>
-                  <li><a href="##">Quick edit</a></li>
-                  <li><a href="##">Trash</a></li>
-                </ul>
-              </span>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <label class="users-table__checkbox">
-                <input type="checkbox" class="check">
-                <div class="categories-table-img">
-                  <picture>
-                    <source srcset="./img/categories/03.webp" type="image/webp"><img src="./img/categories/03.jpg" alt="category">
-                  </picture>
-                </div>
-              </label>
-            </td>
-            <td>
-              Helping a local business reinvent itself
-            </td>
-            <td>
-              <div class="pages-table-img">
-                <picture>
-                  <source srcset="./img/avatar/avatar-face-02.webp" type="image/webp"><img src="./img/avatar/avatar-face-02.png" alt="User Name">
-                </picture>
-                Kathryn Murphy
-              </div>
-            </td>
-            <td><span class="badge-active">Active</span></td>
-            <td>17.04.2021</td>
-            <td>
-              <span class="p-relative">
-                <button class="dropdown-btn transparent-btn" type="button" title="More info">
-                  <div class="sr-only">More info</div>
-                  <i data-feather="more-horizontal" aria-hidden="true"></i>
-                </button>
-                <ul class="users-item-dropdown dropdown">
-                  <li><a href="##">Edit</a></li>
-                  <li><a href="##">Quick edit</a></li>
-                  <li><a href="##">Trash</a></li>
-                </ul>
-              </span>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <label class="users-table__checkbox">
-                <input type="checkbox" class="check">
-                <div class="categories-table-img">
-                  <picture>
-                    <source srcset="./img/categories/04.webp" type="image/webp"><img src="./img/categories/04.jpg" alt="category">
-                  </picture>
-                </div>
-              </label>
-            </td>
-            <td>
-              Caring is the new marketing
-            </td>
-            <td>
-              <div class="pages-table-img">
-                <picture>
-                  <source srcset="./img/avatar/avatar-face-05.webp" type="image/webp"><img src="./img/avatar/avatar-face-05.png" alt="User Name">
-                </picture>
-                Guy Hawkins
-              </div>
-            </td>
-            <td><span class="badge-active">Active</span></td>
-            <td>17.04.2021</td>
-            <td>
-              <span class="p-relative">
-                <button class="dropdown-btn transparent-btn" type="button" title="More info">
-                  <div class="sr-only">More info</div>
-                  <i data-feather="more-horizontal" aria-hidden="true"></i>
-                </button>
-                <ul class="users-item-dropdown dropdown">
-                  <li><a href="##">Edit</a></li>
-                  <li><a href="##">Quick edit</a></li>
-                  <li><a href="##">Trash</a></li>
-                </ul>
-              </span>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <label class="users-table__checkbox">
-                <input type="checkbox" class="check">
-                <div class="categories-table-img">
-                  <picture>
-                    <source srcset="./img/categories/01.webp" type="image/webp"><img src="./img/categories/01.jpg" alt="category">
-                  </picture>
-                </div>
-              </label>
-            </td>
-            <td>
-              How to build a loyal community online and offline
-            </td>
-            <td>
-              <div class="pages-table-img">
-                <picture>
-                  <source srcset="./img/avatar/avatar-face-03.webp" type="image/webp"><img src="./img/avatar/avatar-face-03.png" alt="User Name">
-                </picture>
-                Robert Fox
-              </div>
-            </td>
-            <td><span class="badge-active">Active</span></td>
-            <td>17.04.2021</td>
-            <td>
-              <span class="p-relative">
-                <button class="dropdown-btn transparent-btn" type="button" title="More info">
-                  <div class="sr-only">More info</div>
-                  <i data-feather="more-horizontal" aria-hidden="true"></i>
-                </button>
-                <ul class="users-item-dropdown dropdown">
-                  <li><a href="##">Edit</a></li>
-                  <li><a href="##">Quick edit</a></li>
-                  <li><a href="##">Trash</a></li>
-                </ul>
-              </span>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <label class="users-table__checkbox">
-                <input type="checkbox" class="check">
-                <div class="categories-table-img">
-                  <picture>
-                    <source srcset="./img/categories/03.webp" type="image/webp"><img src="./img/categories/03.jpg" alt="category">
-                  </picture>
-                </div>
-              </label>
-            </td>
-            <td>
-              How to build a loyal community online and offline
-            </td>
-            <td>
-              <div class="pages-table-img">
-                <picture>
-                  <source srcset="./img/avatar/avatar-face-03.webp" type="image/webp"><img src="./img/avatar/avatar-face-03.png" alt="User Name">
-                </picture>
-                Robert Fox
-              </div>
-            </td>
-            <td><span class="badge-active">Active</span></td>
-            <td>17.04.2021</td>
-            <td>
-              <span class="p-relative">
-                <button class="dropdown-btn transparent-btn" type="button" title="More info">
-                  <div class="sr-only">More info</div>
-                  <i data-feather="more-horizontal" aria-hidden="true"></i>
-                </button>
-                <ul class="users-item-dropdown dropdown">
-                  <li><a href="##">Edit</a></li>
-                  <li><a href="##">Quick edit</a></li>
-                  <li><a href="##">Trash</a></li>
-                </ul>
-              </span>
-            </td>
-          </tr>
+          <?php foreach ($comments as $key => $value) { ?>
+            <tr>
+              <td>
+                <img src="<?php echo "../" . $value["user_image"] ?>" alt=""> <?php echo $value["user_name"] ?>
+              </td>
+              <td>
+                <?php echo $value["comment"] ?>
+              </td>
+              <td>
+                <?php if ($value["comment_image"] != NULL) { ?>
+                  <img src="<?php echo $value["comment_image"] ?>">
+                <?php } else {
+                  echo "No image";
+                } ?>
+              </td>
+              <td><?php echo $value["comment_date"] ?></td>
+              <td>
+                <span class="p-relative">
+                  <button class="dropdown-btn transparent-btn" type="button" title="More info">
+                    <div class="sr-only">More info</div>
+                    <i data-feather="more-horizontal" aria-hidden="true"></i>
+                  </button>
+                  <ul class="users-item-dropdown dropdown">
+                    <li><a href="?do=delete&id=<?php echo $value["comment_id"] ?>">Trash</a></li>
+                  </ul>
+                </span>
+              </td>
+            </tr>
+          <?php } ?>
         </tbody>
       </table>
     </div>
   </div>
 </div>
+<!-- end table -->
 <?php include "./includes/footer.php"; ?>
