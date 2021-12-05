23c94492fd2052223b8e1ca0691d8905a33d8834
haithamassoli <haitham.b.assoli@gmail.com>
2021-12-05 09:35:47 +0200

    styling

--- code/includes/header.php
+++ code/includes/header.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -38,31 +39,32 @@
 </head>
 
 <body class="animsition">
-
 	<!-- Header -->
-	<header class="header-v3">
+	<header>
 		<!-- Header desktop -->
-		<div class="container-menu-desktop trans-03">
+		<div class="container-menu-desktop">
 			<div class="wrap-menu-desktop">
-				<nav class="limiter-menu-desktop p-l-45">
+				<nav class="limiter-menu-desktop container">
 
 					<!-- Logo desktop -->
 					<a href="#" class="logo">
-						<img src="images/icons/logo-02.png" alt="IMG-LOGO">
+						<img src="images/icons/logo-01.png" alt="IMG-LOGO">
 					</a>
 
 					<!-- Menu desktop -->
 					<div class="menu-desktop">
 						<ul class="main-menu">
-							<li>
+							<li class="active-menu">
 								<a href="index.php">Home</a>
 							</li>
+
 							<li>
 								<a href="shop.php">Shop</a>
 							</li>
 							<li>
 								<a href="about.php">About</a>
 							</li>
+
 							<li>
 								<a href="contact.php">Contact</a>
 							</li>
@@ -70,12 +72,37 @@
 					</div>
 
 					<!-- Icon header -->
-					<div class="wrap-icon-header flex-w flex-r-m h-full">
-						<div class="flex-c-m h-full p-r-25 bor6">
-							<div class="icon-header-item cl0 hov-cl1 trans-04 p-lr-11 icon-header-noti js-show-cart" data-notify="2">
-								<i class="zmdi zmdi-shopping-cart"></i>
+					<div class="wrap-icon-header flex-w flex-r-m">
+						<?php if (!isset($_SESSION['type'])) { ?>
+							<div class="cl2 hov-cl1 trans-04 p-l-22 p-r-11" style="cursor: pointer;">
+								LOGIN
 							</div>
+						<?php } elseif ($_SESSION['type'] == 0) { ?>
+							<div class="cl2 hov-cl1 trans-04 p-l-22 p-r-11" style="cursor: pointer;">
+								Profile
+							</div>
+							<div class="cl2 hov-cl1 trans-04 p-l-22 p-r-11" style="cursor: pointer;">
+								Logout
+							</div>
+						<?php } elseif ($_SESSION['type'] == 0 || $_SESSION['type'] == 2) { ?>
+							<div class="cl2 hov-cl1 trans-04 p-l-22 p-r-11" style="cursor: pointer;">
+								DashBoard
+							</div>
+							<div class="cl2 hov-cl1 trans-04 p-l-22 p-r-11" style="cursor: pointer;">
+								Logout
+							</div>
+						<?php } ?>
+						<div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-modal-search">
+							<i class="zmdi zmdi-search"></i>
 						</div>
+
+						<div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart" data-notify="2">
+							<i class="zmdi zmdi-shopping-cart"></i>
+						</div>
+
+						<a href="#" class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti" data-notify="0">
+							<i class="zmdi zmdi-favorite-outline"></i>
+						</a>
 					</div>
 				</nav>
 			</div>
@@ -89,12 +116,18 @@
 			</div>
 
 			<!-- Icon header -->
-			<div class="wrap-icon-header flex-w flex-r-m h-full m-r-15">
-				<div class="flex-c-m h-full p-r-5">
-					<div class="icon-header-item cl2 hov-cl1 trans-04 p-lr-11 icon-header-noti js-show-cart" data-notify="2">
-						<i class="zmdi zmdi-shopping-cart"></i>
-					</div>
+			<div class="wrap-icon-header flex-w flex-r-m m-r-15">
+				<div class="icon-header-item cl2 hov-cl1 trans-04 p-r-11 js-show-modal-search">
+					<i class="zmdi zmdi-search"></i>
 				</div>
+
+				<div class="icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti js-show-cart" data-notify="2">
+					<i class="zmdi zmdi-shopping-cart"></i>
+				</div>
+
+				<a href="#" class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti" data-notify="0">
+					<i class="zmdi zmdi-favorite-outline"></i>
+				</a>
 			</div>
 
 			<!-- Button show menu -->
@@ -108,9 +141,40 @@
 
 		<!-- Menu Mobile -->
 		<div class="menu-mobile">
+			<ul class="topbar-mobile">
+				<li>
+					<div class="left-top-bar">
+						Free shipping for standard order over $100
+					</div>
+				</li>
+
+				<li>
+					<div class="right-top-bar flex-w h-full">
+						<a href="#" class="flex-c-m p-lr-10 trans-04">
+							Help & FAQs
+						</a>
+
+						<a href="#" class="flex-c-m p-lr-10 trans-04">
+							My Account
+						</a>
+
+						<a href="#" class="flex-c-m p-lr-10 trans-04">
+							EN
+						</a>
+
+						<a href="#" class="flex-c-m p-lr-10 trans-04">
+							USD
+						</a>
+					</div>
+				</li>
+			</ul>
+
 			<ul class="main-menu-m">
 				<li>
 					<a href="index.php">Home</a>
+					<span class="arrow-main-menu-m">
+						<i class="fa fa-angle-right" aria-hidden="true"></i>
+					</span>
 				</li>
 
 				<li>
@@ -129,22 +193,20 @@
 
 		<!-- Modal Search -->
 		<div class="modal-search-header flex-c-m trans-04 js-hide-modal-search">
-			<button class="flex-c-m btn-hide-modal-search trans-04">
-				<i class="zmdi zmdi-close"></i>
-			</button>
+			<div class="container-search-header">
+				<button class="flex-c-m btn-hide-modal-search trans-04 js-hide-modal-search">
+					<img src="images/icons/icon-close2.png" alt="CLOSE">
+				</button>
 
-			<form class="container-search-header">
-				<div class="wrap-search-header">
-					<input class="plh0" type="text" name="search" placeholder="Search...">
-
+				<form class="wrap-search-header flex-w p-l-15" method="post">
 					<button class="flex-c-m trans-04">
 						<i class="zmdi zmdi-search"></i>
 					</button>
-				</div>
-			</form>
+					<input class="plh3" type="search" name="search" placeholder="Search...">
+				</form>
+			</div>
 		</div>
 	</header>
-
 
 	<!-- Cart -->
 	<div class="wrap-header-cart js-panel-cart">
@@ -230,5 +292,3 @@
 			</div>
 		</div>
 	</div>
-	<!-- end of cart  -->
-	<!-- ----------------------------------------------------------------------  -->
